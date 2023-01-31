72c5287bd7df92827ebd54571160f0f2d4a62e54
Jake Jepson <jwjepson1@gmail.com>
2023-01-30 23:33:33 -0500

    render header and education sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Header } from "./components/Header";
+import { Education } from "./components/Education";
 
 class App extends React.Component {
   render() {
     return (
-      <Header/>
+      <>
+        <Header/>
+        <Education/>
+      </>
     );
   }
 }
