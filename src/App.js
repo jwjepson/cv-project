d6b0db58d7a3c724dff599330112ce4f97592e29
Jake Jepson <jwjepson1@gmail.com>
2023-02-01 22:37:19 -0500

    import the Experience section component to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { Education } from "./components/Education";
+import { Experience } from "./components/Experience";
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <>
         <Header/>
         <Education/>
+        <Experience/>
       </>
     );
   }
