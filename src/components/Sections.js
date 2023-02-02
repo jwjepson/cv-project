dd9c7e8ad975453d1556ee2ae694f4bbc2f24d7a
Jake Jepson <jwjepson1@gmail.com>
2023-02-01 22:38:07 -0500

    create section headers with buttons using a component

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -2,11 +2,12 @@ import React from "react";
 
 class Sections extends React.Component {
     render() {
-        const {title, handleEdit} = this.props;
+        const {title, handleEdit} = this.props;     
         return(
-            <div class="sectionHeader">
+            <div className="sectionHeader">
                 <h2>{title}</h2>
-                <button onClick={handleEdit} className="sectionEditButton">Edit</button>
+                <button onClick={handleEdit} className="sectionEditButton">Add {title}</button>
+                <button>Edit</button>
             </div>
         )
     }
