6786bdd57ec095899aca8079528bc1fada88bd93
Jake Jepson <jwjepson1@gmail.com>
2023-02-01 22:40:00 -0500

    create an overview component that displays each job with delete and edit buttons

---
+++ src/components/Overview.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+class Overview extends React.Component {
+    render() {
+        const {jobs, onDelete} = this.props;
+        const reversedJobs = jobs.slice().reverse();
+        console.log(jobs);
+        return (
+            <>
+            {reversedJobs.map((job) => {
+                return (
+                    <div key={job.id} className="job">
+                        <div>
+                            <div className="companyName">{job.company}</div>
+                            <div>{job.position}</div>
+                            <div>{job.tasks}</div>
+                            <div>{job.startDate} {job.endDate}</div>
+                        </div>
+                        <div className="jobButtons">
+                            <button value={job.id} onClick={onDelete}>Delete</button>
+                            <button value={job.id}>Edit</button>
+                        </div>
+                    </div>
+                )
+            })}
+            </>
+        )
+    }
+}
+
+export {Overview}
